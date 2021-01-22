882d5175f0535dd82c4f5b35246cb8e92f2c7544
Paulo Oliveira <paulosoujava@gmail.com>
2021-01-22 19:24:23 -0300

    ensure return null if returns 204 with  body

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -17,7 +17,12 @@ class HttpAdapter implements HttpClient {
     final headers = {'content-tye': 'application/json', 'accept': 'application/json'};
     final jsonBody = body != null ? jsonEncode(body) : null;
     final response = await client.post(url, headers: headers, body: jsonBody);
-    return response.body.isEmpty ? null : jsonDecode(response.body);
+
+    if (response.statusCode == 200) {
+      return response.body.isEmpty ? null : jsonDecode(response.body);
+    } else {
+      return null;
+    }
   }
 }
 
@@ -88,7 +93,17 @@ void main() {
     });
 
     test("Should return  null if post returns 204", () async {
-      mockResponse(200, body: '');
+      mockResponse(204, body: '');
+      final response = await sut.request(
+        url: url,
+        method: 'post',
+      );
+
+      expect(response, null);
+    });
+
+    test("Should return  null if post returns 204 with data", () async {
+      mockResponse(204);
       final response = await sut.request(
         url: url,
         method: 'post',
