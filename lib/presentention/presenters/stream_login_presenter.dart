d1065acd64a4c3b0e0c27b1c001bce43de577510
Paulo Oliveira <paulosoujava@gmail.com>
2021-01-24 18:51:02 -0300

    Should not emit after dispose

--- lib/presentention/presenters/stream_login_presenter.dart
+++ lib/presentention/presenters/stream_login_presenter.dart
@@ -20,18 +20,18 @@ class LoginState {
 class StreamLoginPresenter {
   final Validation validation;
   final Authentication authentication;
-  final _controller = StreamController<LoginState>.broadcast();
+  var _controller = StreamController<LoginState>.broadcast();
   var _state = LoginState();
 
   StreamLoginPresenter({@required this.validation, @required this.authentication});
 
-  Stream<String> get emailErrorStream => _controller.stream.map((state) => state.emailError).distinct();
-  Stream<String> get passwordErrorStream => _controller.stream.map((state) => state.passwordError).distinct();
-  Stream<String> get mainErrorStream => _controller.stream.map((state) => state.mainError).distinct();
-  Stream<bool> get isFormValidStream => _controller.stream.map((state) => state.isFormValid).distinct();
-  Stream<bool> get isLoadStream => _controller.stream.map((state) => state.isLoad).distinct();
+  Stream<String> get emailErrorStream => _controller?.stream?.map((state) => state.emailError)?.distinct();
+  Stream<String> get passwordErrorStream => _controller?.stream?.map((state) => state.passwordError)?.distinct();
+  Stream<String> get mainErrorStream => _controller?.stream?.map((state) => state.mainError)?.distinct();
+  Stream<bool> get isFormValidStream => _controller?.stream?.map((state) => state.isFormValid)?.distinct();
+  Stream<bool> get isLoadStream => _controller?.stream?.map((state) => state.isLoad)?.distinct();
 
-  void _update() => _controller.add(_state);
+  void _update() => _controller?.add(_state);
 
   void validateEmail(String email) {
     _state.email = email;
@@ -54,6 +54,11 @@ class StreamLoginPresenter {
     }
 
     _toggeLoading(false);
+  }
+
+  void dispose() {
+    _controller?.close();
+    _controller = null;
   }
 
   _toggeLoading(bool isShow) {
