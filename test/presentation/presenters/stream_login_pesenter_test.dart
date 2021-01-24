9048bd216cb0c3a4e8271832183ac4a6ed1ba862
Paulo Oliveira <paulosoujava@gmail.com>
2021-01-24 17:29:12 -0300

    stream login presenter emits form valid events

--- test/presentation/presenters/stream_login_pesenter_test.dart
+++ test/presentation/presenters/stream_login_pesenter_test.dart
@@ -100,4 +100,15 @@ void main() {
     sut.validateEmail(email);
     sut.validatePassword(password);
   });
+
+  test('Should emit error only password input and email is ok', () async {
+    sut.emailErrorStream.listen(expectAsync1((error) => expect(error, null)));
+    sut.passwordErrorStream.listen(expectAsync1((error) => expect(error, null)));
+
+    expectLater(sut.isFormValidStream, emitsInOrder([false, true]));
+
+    sut.validateEmail(email);
+    await Future.delayed(Duration.zero);
+    sut.validatePassword(password);
+  });
 }
